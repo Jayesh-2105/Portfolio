68cd6f121aff5958bb5981b914596b327b794833
jayesh bisen <jayeshbisen@jayeshs-MacBook-Air.local>
2025-08-01 00:54:49 +0530

    Initial commit with cleaned project

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    darkMode : 'media',
+    content: ["./index.html"], // or ./src/**/*.{html,js} for nested files
+    theme: {
+      extend: {},
+    },
+    
+    plugins: [],
+  }
